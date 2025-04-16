ca3fd9d38a0a6edcf0ab5c41cb06d9f623539a77
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2025-04-16 18:52:21 -0300

    chore: adjust ingress

--- main.tf
+++ main.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "vpc_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 587
+    to_port     = 587
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
